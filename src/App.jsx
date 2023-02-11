5b3eb1720fb63cfdd3d74cdc56dab486f035080f
JOshi kv <joshikv3705@gmail.com>
2023-02-11 12:46:23 +0530

    image poster design

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import './Components/NavBar/Navbar.css'
 import './App.css'
 import Banner from "./Components/Banner/Banner";
 import './Components/Banner/Banner.css'
+import Rowpost from "./Components/RowPost/Rowpost";
+import './Components/RowPost/Rowpost.css'
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Banner/>
+     <Rowpost/>
     </div>
   );
 }
