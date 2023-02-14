fd7f5aace5c3429d92a04e8472fb6ec1e6cc2340
JOshi kv <joshikv3705@gmail.com>
2023-02-14 14:59:40 +0530

    rowposter,url changing dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import './App.css'
 import Banner from "./Components/Banner/Banner";
 import './Components/Banner/Banner.css'
 import Rowpost from "./Components/RowPost/Rowpost";
+import {orginals,action} from './Constants/urls'
 import './Components/RowPost/Rowpost.css'
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Banner/>
-     <Rowpost/>
+     <Rowpost url={orginals} title='Netflix orginals'/>
+     <Rowpost url={action} title='Action' isSmall/>
     </div>
   );
 }
