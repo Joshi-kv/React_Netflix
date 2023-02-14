b733226c1511d40ef6357bf5e786921003ae77dd
JOshi kv <joshikv3705@gmail.com>
2023-02-14 12:00:22 +0530

    banner data changing dynamically

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect,useState } from 'react'
 import axios from '../../axios'
-import {API_KEY} from '../../Constants/constants'
+import {API_KEY,imageUrl} from '../../Constants/constants'
 function Banner() {
-
+  const [movie,setMovie]=useState()
   useEffect(()=>{
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-      console.log(response.data.results[0]);
+      setMovie(response.data.results[0])
     })
   },[])
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}} className='banner'>
       <div className="content">
-        <h1 className="title">Movie Name</h1>
+        <h1 className="title">{movie ? movie.title : ''}</h1>
         <div className="banner-button">
             <button className='button'>Play</button>
             <button className='button'>Movie List</button>
         </div>
         <h1 className="description">
-           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+           {movie ? movie.overview : ""} 
         </h1>
       </div>
       <div className="fade"></div>
--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl='https://api.themoviedb.org/3'
 export const API_KEY='ea1c21e8d4a9cbfe2eebb0ea41f3192a'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
